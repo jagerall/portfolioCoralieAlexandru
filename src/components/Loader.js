77c17cd3f6d4afd29abeab492d8d3873a477b5e4
coraliealexandru <coralie.alexandru@4sh.fr>
2025-01-26 20:02:49 +0100

    style loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,11 @@
-
 import '../style/loader.scss';
 
 const Loader = () => {
     return (
         <div className="loader">
-            <div className="loader-spinner"></div>
+            <div className="loader-spinner">
+                <img className="loader-spinner-image" src='../assets/spirale.gif' alt="Illustration Coralie"></img>
+            </div>
         </div>
     );
 };
--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import PageContent from '../components/pageContent';
@@ -13,51 +13,53 @@ function App() {
     useEffect(() => {
         setTimeout(() => {
             setLoading(false);
-        }, 1000);
+        }, 1700);
     }, []);
 
     if (loading) {
-        return <Loader />;
+        return <Loader/>;
     }
 
     return (
         <Router>
-            <Header />
-            <Routes>
-                <Route path="/uiux" element={<PageContent category="uiux" />} />
-                <Route path="/da" element={<PageContent category="da" />} />
-                <Route path="/photo" element={<PageContent category="photo" />} />
-                <Route path="/illu" element={<PageContent category="illu" />} />
-                <Route path="/projet/:id" element={<ProjectPage />} />
-                <Route
-                    path="/mentions-legales"
-                    element={
-                        <StaticPage
-                            title="Mentions légales"
-                            content={<p>Voici les mentions légales du site.</p>}
-                        />
-                    }
-                />
-                <Route
-                    path="/about"
-                    element={
-                        <StaticPage
-                            title="À propos de moi"
-                            content={<div><p>Bonjour ! Je suis Coralie...</p></div>}
-                        />
-                    }
-                />
-                <Route
-                    path="/CV"
-                    element={
-                        <StaticPage
-                            title="CV"
-                            content={<div><p>CV</p></div>}
-                        />
-                    }
-                />
-            </Routes>
-            <Footer />
+            <div className="page-content">
+                <Header/>
+                <Routes>
+                    <Route path="/uiux" element={<PageContent category="uiux"/>}/>
+                    <Route path="/da" element={<PageContent category="da"/>}/>
+                    <Route path="/photo" element={<PageContent category="photo"/>}/>
+                    <Route path="/illu" element={<PageContent category="illu"/>}/>
+                    <Route path="/projet/:id" element={<ProjectPage/>}/>
+                    <Route
+                        path="/mentions-legales"
+                        element={
+                            <StaticPage
+                                title="Mentions légales"
+                                content={<p>Voici les mentions légales du site.</p>}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/about"
+                        element={
+                            <StaticPage
+                                title="À propos de moi"
+                                content={<div><p>Bonjour ! Je suis Coralie...</p></div>}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/CV"
+                        element={
+                            <StaticPage
+                                title="CV"
+                                content={<div><p>CV</p></div>}
+                            />
+                        }
+                    />
+                </Routes>
+                <Footer/>
+            </div>
         </Router>
     );
 }
