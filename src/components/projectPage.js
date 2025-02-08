f3948c2d7c32021485f9291db08a27dac7310e24
coraliealexandru <coralie.alexandru@4sh.fr>
2025-02-08 23:44:50 +0100

    adding stuff and layout

--- src/components/projectPage.js
+++ src/components/projectPage.js
@@ -1,66 +1,125 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
-import '../style/projectpage.scss';
-import Button from "./button";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import Button from "./button";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import '../style/projectpage.scss';
+import {EffectCards, Pagination, Navigation, Mousewheel,Keyboard} from 'swiper/modules';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const projectDetails = {
     smash: {
         title: 'Smash',
         description: 'Smash est un média destiné aux 18-25 ans. Il propose de lutter contre la désinformation. \n' +
-            ' Tous les jours, l\'utilisateur a accès à 3 citations de personnes publiques sur des sujets d\'actualité et doit déterminer si c\'est une info (basé sur des faits) ou une intox (basé sur une opinion ou une interprétation des faits). Il peut ensuite s\'informer de façon plurielle sur cette actualité et en débattre dans un espace commentaire.\n' +
+            'Tous les jours, l\'utilisateur a accès à 3 citations de personnes publiques sur des sujets d\'actualité et doit déterminer si c\'est une info (basé sur des faits) ou une intox (basé sur une opinion ou une interprétation des faits). Il peut ensuite s\'informer de façon plurielle sur cette actualité et en débattre dans un espace commentaire.\n' +
             'Notre application avait plusieurs besoins : des comptes utilisateurs, un système de sondage, d\'amis et de commentaires ainsi que la création et gestion d\'articles.',
-        images: ['/assets/ui-smash.svg', '/assets/smash-1.png', '/assets/smash-2.png', '/assets/smash-3.png'],
+        images: ['/assets/smash-1.png', '/assets/smash-2.png', '/assets/smash-3.png'],
         linkFigma: 'https://www.figma.com/design/iuAP57wyrjMOlp2AdZuRLy/SMASH?m=auto&t=6La0QN4LOZ35hia1-6',
-        linkWebsite: 'https://flip-app.osc-fr1.scalingo.io/'
     },
-
     flop: {
         title: 'Flop\'EDT',
         description: 'Audit ergonomique & refonte du système de gestion de planning',
         images: ['/assets/flop-1.png', '/assets/flop-2.png', '/assets/flop-3.png'],
         linkFigma: 'https://www.figma.com/design/9fY2i3PUefoq9BwAPgPAYa/Refonte-Flop!Edt',
     },
-
     argentique: {
         title: 'Photographie argentique',
         description: 'Voici quelques photos prises à l\'Olympus M1 sur la Côte d\'Azur.',
         images: ['/assets/argentique-5.jpg', '/assets/argentique-2.jpg', '/assets/argentique-3.jpg', '/assets/argentique-4.jpg', '/assets/argentique-1.jpg', '/assets/argentique-6.jpg'],
     },
 };
+
 const ProjectPage = () => {
     const { id } = useParams();
     const navigate = useNavigate();
     const project = projectDetails[id] || {};
 
+    // Etat pour contrôler l'ouverture du modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }, []);
 
+    // Fonction pour ouvrir le modal
+    const openModal = (index) => {
+        setCurrentImageIndex(index);
+        setIsModalOpen(true);
+    };
+
+    // Fonction pour fermer le modal
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <main className="project-page">
             <div className="project-description">
-                <h1>{project.title}</h1>
-                <p>{project.description}</p>
-                <div className="project-links">
-                    {project.linkFigma && (
-                        <Button to={project.linkFigma} external={true} label="Voir le Figma"/>
-                    )}
-                    {project.linkWebsite && (
-                        <Button to={project.linkWebsite} external={true} label="Voir le site"/>
-                    )}
+                <div className="project-description-head">
+                    <h1>{project.title}</h1>
+                    <div className="project-links">
+                        {project.linkFigma && (
+                            <Button to={project.linkFigma} external={true} label="Voir le Figma" />
+                        )}
+                    </div>
                 </div>
+                <p>{project.description}</p>
             </div>
 
             <div className="project-images">
-                {project.images && project.images.map((image, index) => (
-                    <img key={index} src={image} alt={`${project.title} ${index + 1}`}/>
-                ))}
+                <Swiper
+                    effect={'cards'}
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    className="mySwiper"
+                >
+                    {project.images && project.images.map((image, index) => (
+                        <SwiperSlide key={index}>
+                            <img
+                                src={image}
+                                alt={`${project.title} ${index + 1}`}
+                                onClick={() => openModal(index)}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
 
-            <button onClick={() => navigate(-1)} className="button">Retour aux projets</button>
+            {/* Modal Fullscreen */}
+            {isModalOpen && (
+                <div className="modal-overlay" onClick={closeModal}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="close-modal" onClick={closeModal}>
+                            <FontAwesomeIcon icon={faXmark} />
+                        </button>
+
+                        <Swiper
+                            cssMode={true}
+                            navigation={true}
+                            pagination={true}
+                            mousewheel={true}
+                            keyboard={true}
+                            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                            className="fullscreen-swiper"
+                        >
+                            {project.images.map((image, index) => (
+                                <SwiperSlide key={index}>
+                                    <img src={image} alt={`${project.title} Fullscreen ${index + 1}`} />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+
+                    </div>
+
+                </div>
+            )}
+
+            <Button onClick={() => navigate(-1)} label={"Retour aux projets"}></Button>
         </main>
     );
 };
+
 export default ProjectPage;
