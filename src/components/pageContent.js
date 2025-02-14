59e76436e48e86896f1819416a2bff47fd8cf4fc
coraliealexandru <coralie.alexandru@4sh.fr>
2025-02-14 18:07:30 +0100

    fix build

--- src/components/pageContent.js
+++ src/components/pageContent.js
@@ -1,73 +1,55 @@
-import React, {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from './card';
 import '../style/pageContent.scss';
 
-const PageContent = ({category}) => {
+const categoryTitles = {
+    uiux: "UI/UX",
+    photo: "Photographie",
+    illu: "Illustrations",
+    autre: "Autres",
+    about: "À propos",
+};
+
+const PageContent = ({ category }) => {
     const navigate = useNavigate();
 
     const categoryData = {
         uiux: [
-            {
-                id: 'pasnumerise',
-                label: 'Nous n\'avons pas numérisé',
-                image: '/assets/ui-pasnumerise.svg',
-                category: 'uiux',
-                type: 'Retour d\'expérience'
-            },
-            {id: 'flop', label: 'FlopEdt', image: '/assets/ui-flop.svg', category: 'uiux', type: 'Audit UX & UI'},
-            {id: 'maria', label: 'Maria', image: '/assets/ui-maria.svg', category: 'uiux', type: 'Narration interactive Musba'},
-            {id: 'capc', label: 'CAPC', image: '/assets/ui-capc.webp', category: 'uiux', type: 'Refonte de site'},
-            {id: 'smash', label: 'Smash', image: '/assets/ui-smash.svg', category: 'uiux', type: 'Application'},
-            {id: 'reserve', label: 'La réserve - Musba', image: '/assets/ui-reserve.svg', category: 'uiux', type: 'Narration interactive Musba'},
+            { id: 'pasnumerise', label: 'Nous n\'avons pas numérisé', image: '/assets/ui-pasnumerise.svg', category: 'uiux', type: 'Retour d\'expérience' },
+            { id: 'flop', label: 'FlopEdt', image: '/assets/ui-flop.svg', category: 'uiux', type: 'Audit UX & UI' },
+            { id: 'maria', label: 'Maria', image: '/assets/ui-maria.svg', category: 'uiux', type: 'Narration interactive Musba' },
+            { id: 'capc', label: 'CAPC', image: '/assets/ui-capc.webp', category: 'uiux', type: 'Refonte de site' },
+            { id: 'smash', label: 'Smash', image: '/assets/ui-smash.svg', category: 'uiux', type: 'Application' },
+            { id: 'reserve', label: 'La réserve - Musba', image: '/assets/ui-reserve.svg', category: 'uiux', type: 'Narration interactive Musba' },
         ],
         photo: [
-            {id: 'argentique', label: 'Argentique', image: '/assets/photo-argentique.webp', category: 'photo', type: 'Èze'},
-            {id: 'auto', label: 'Autoportrait', image: '/assets/photo-auto.webp', category: 'photo', type: 'Coralie'},
-            {id: 'faune', label: 'Faune', image: '/assets/photo-animaux.webp', category: 'photo', type: 'Zoo de la Palmyre'},
-            {id: 'bordeaux', label: 'Bordeaux', image: '/assets/photo-bordeaux.webp', category: 'photo', type: 'Bordeaux'},
-            {id: 'macro', label: 'Macro', image: '/assets/photo-macro.webp', category: 'photo', type: 'Vendée'},
+            { id: 'argentique', label: 'Argentique', image: '/assets/photo-argentique.webp', category: 'photo', type: 'Èze' },
+            { id: 'auto', label: 'Autoportrait', image: '/assets/photo-auto.webp', category: 'photo', type: 'Coralie' },
+            { id: 'faune', label: 'Faune', image: '/assets/photo-animaux.webp', category: 'photo', type: 'Zoo de la Palmyre' },
+            { id: 'bordeaux', label: 'Bordeaux', image: '/assets/photo-bordeaux.webp', category: 'photo', type: 'Bordeaux' },
+            { id: 'macro', label: 'Macro', image: '/assets/photo-macro.webp', category: 'photo', type: 'Vendée' },
         ],
         illu: [
-            {id: 'allo', label: 'Allô à l\'huile', image: '/assets/illu-allo.webp', category: 'illu', type: 'Design coque de téléphone'},
-            {id: 'logommi', label: 'Logo MMI', image: '/assets/illu-logommi.webp', category: 'illu', type: 'Logo'},
-            {id: 'miel', label: 'Miel de 4SH', image: '/assets/illu-miel.webp', category: 'illu', type: 'Étiquette '},
-            {id: 'art', label: 'Art', image: '/assets/illu-art.webp', category: 'illu', type: 'Essais tableaux'},
-            {id: 'Amis', label: 'Amis', image: '/assets/illu-ami.webp', category: 'illu', type: 'Dessin numérique'},
-            {id: 'pieuvre', label: 'Pieuvre', image: '/assets/illu-pieuvre.webp', category: 'illu', type: 'Dessin numérique'},
-            {id: 'cocktails', label: 'Cocktails', image: '/assets/illu-cocktails.webp', category: 'illu', type: 'Dessin numérique'},
+            { id: 'allo', label: 'Allô à l\'huile', image: '/assets/illu-allo.webp', category: 'illu', type: 'Design coque de téléphone' },
+            { id: 'logommi', label: 'Logo MMI', image: '/assets/illu-logommi.webp', category: 'illu', type: 'Logo' },
+            { id: 'miel', label: 'Miel de 4SH', image: '/assets/illu-miel.webp', category: 'illu', type: 'Étiquette ' },
+            { id: 'art', label: 'Art', image: '/assets/illu-art.webp', category: 'illu', type: 'Essais tableaux' },
+            { id: 'Amis', label: 'Amis', image: '/assets/illu-ami.webp', category: 'illu', type: 'Dessin numérique' },
+            { id: 'pieuvre', label: 'Pieuvre', image: '/assets/illu-pieuvre.webp', category: 'illu', type: 'Dessin numérique' },
+            { id: 'cocktails', label: 'Cocktails', image: '/assets/illu-cocktails.webp', category: 'illu', type: 'Dessin numérique' },
         ],
         autre: [
-            {
-                id: 'mainslibres',
-                label: 'Les Mains Libres',
-                image: '/assets/mainslibres.webp',
-                category: 'autre',
-                type: 'Direction Artistique'
-            },
-            {
-                id: 'motion',
-                label: 'Distortions cognitives',
-                image: '/assets/motion.webp',
-                category: 'autre',
-                type: 'Motion Design'
-            },
+            { id: 'mainslibres', label: 'Les Mains Libres', image: '/assets/mainslibres.webp', category: 'autre', type: 'Direction Artistique' },
+            { id: 'motion', label: 'Distortions cognitives', image: '/assets/motion.webp', category: 'autre', type: 'Motion Design' },
         ],
     };
 
     const projects = categoryData[category] || [];
 
-    const categoryTitles = {
-        uiux: "UI/UX",
-        photo: "Photographie",
-        illu: "Illustrations",
-        autre: "Autres",
-        about: "À propos",
-    };
-
     useEffect(() => {
-        document.title = `${categoryTitles[category] || "Coralie Alexandru "}`;
-    }, [category, categoryTitles]);
+        document.title = categoryTitles[category] || "Coralie Alexandru";
+    }, [category]);
 
     return (
         <main className="page-content">
@@ -78,7 +60,7 @@ const PageContent = ({category}) => {
                         image={project.image}
                         label={project.label}
                         onClick={() => navigate(`/${project.category}/${project.id}`)}
-                        type={(project.type)}
+                        type={project.type}
                     />
                 ))}
             </div>
