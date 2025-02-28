a81eb5df56464026962de4f2af41eb8455ce16b5
coraliealexandru <coralie.alexandru@4sh.fr>
2025-02-28 13:31:19 +0100

    fix video

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,8 +4,11 @@ const Loader = () => {
     return (
         <div className="loader">
             <div className="loader-spinner">
-                <img className="loader-spinner-image" src='../../public/assets/spirale.mp4' alt="Illustration Coralie"></img>
+                <video className="loader-spinner-video" autoPlay loop muted>
+                    <source src="/assets/spirale.mp4" type="video/mp4" />
+                </video>
             </div>
+
         </div>
     );
 };
